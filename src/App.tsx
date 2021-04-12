681147ba3a00c9e57d1d3b7958daa7129542573d
Emincan Demirkaya <emincan@fireflyon.com>
2021-04-13 02:34:52 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useMemo } from "react";
-import { useEffect, useState } from "react";
-import "./App.scss";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import cities from "./data/ukcities.json";
-import "bootstrap/dist/css/bootstrap.css";
 import * as moment from "moment";
+import "./App.scss";
+import cities from "./data/ukcities.json";
+import { useGA4React } from "ga-4-react";
 
 let apiKey = "3d7132f0e89f9435962d14739075d98d";
 
@@ -12,8 +11,12 @@ function App() {
   const [place, setPlace] = useState<any>("London");
   // const [lon, setLon] = useState<any>(-0.33333);
   const [data, setData] = useState<any>(null);
+
+  const ga = useGA4React();
+
   // let url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&APPID=${apiKey}&units=metric`;
 
+  // http://api.openweathermap.org/data/2.5/air_pollution/history?lat=508&lon=50&start=1606223802&end=1606482999&appid={APIkey}
   const getList = async (place: string) => {
     await axios
       .get(
@@ -34,73 +37,95 @@ function App() {
     console.log(data);
   }, [place]);
 
+  cities.sort(function(a: any, b: any) {
+    a = a.name.toLowerCase();
+    b = b.name.toLowerCase();
+
+    return a < b ? -1 : a > b ? 1 : 0;
+  });
+
   return (
     <>
-      <div className="container">
-        <div className="row align-items-start">
-          <div className="my-5 col-3">
-            <div className="d-flex justify-center">
-              <div className="shadow rounded-lg w-full h-600 overflow-auto">
-                <ul className="p-4">
-                  {cities.map((city, index) => (
-                    <li
-                      className="p-1 cursor-pointer"
-                      key={index}
-                      onClick={() => {
-                        setPlace(city.name);
-                      }}
-                    >
-                      {city.name}, {city.subcountry}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+      <div className="row align-items-start">
+        <div className="my-5 col-4">
+          <div className="d-flex justify-center">
+            <div className="shadow rounded-lg h-600 overflow-auto">
+              <ul className="p-4">
+                {cities.map((city, index) => (
+                  <li
+                    className="d-flex p-1 cursor-pointer align-items-center"
+                    key={index}
+                    onClick={() => {
+                      setPlace(city.name);
+                    }}
+                  >
+                    {city.name}{" "}
+                    <span className="h4">
+                      {city.subcountry == "Scotland"
+                        ? "🏴󠁧󠁢󠁳󠁣󠁴󠁿"
+                        : city.subcountry == "Wales"
+                        ? "🏴󠁧󠁢󠁷󠁬󠁳󠁿"
+                        : city.subcountry == "England"
+                        ? "🏴󠁧󠁢󠁥󠁮󠁧󠁿"
+                        : "🇬🇧"}
+                    </span>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
-          <div className="mt-5 col-9">
-            <div className="shadow rounded-lg overflow-hidden">
-              {data != null ? (
-                <div
-                  className="p-4"
-                  style={
-                    data.clouds.all >= 50 && Date.now() >= data.sys.sunset
-                      ? { backgroundColor: "#C6C6C6" }
-                      : data.clouds.all >= 50 && Date.now() <= data.sys.sunset
-                      ? { backgroundColor: "#9e9e9e" }
-                      : data.clouds.all >= 50 && Date.now() <= data.sys.sunset
-                      ? { backgroundColor: "#0073cc" }
-                      : { backgroundColor: "#0090FF" }
-                  }
-                >
-                  <div className="d-flex align-items-center">
-                    <h1 className="font-weight-bold">{data.name}</h1>
-                    <img
-                      className="ml-auto"
-                      src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                    />
-                  </div>
-                  <div className="d-flex align-items-center desc-text justify-content-around">
-                    <h6>
-                      Sunrise: {moment.unix(data.sys.sunrise).format("LT")}
-                    </h6>
+        </div>
+        <div className="mt-5 col-8">
+          <div className="shadow rounded-lg overflow-hidden">
+            {data != null ? (
+              <div
+                className="p-4"
+                style={
+                  data.clouds.all >= 50
+                    ? Date.now() >= data.sys.sunset
+                      ? { backgroundColor: "#858e96" }
+                      : { backgroundColor: "#d0deec" }
+                    : Date.now() <= data.sys.sunset
+                    ? { backgroundColor: "#87ceeb" }
+                    : { backgroundColor: "#191970" }
+                }
+              >
+                <div className="d-flex align-items-center">
+                  <h1 className="font-weight-bold">{data.name}</h1>
+                  <img
+                    className="ml-auto"
+                    src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                  />
+                </div>
+                <div className="d-flex align-items-center desc-text justify-content-around">
+                  <h6>Sunrise: {moment.unix(data.sys.sunrise).format("LT")}</h6>
 
-                    <h6>Sunset: {moment.unix(data.sys.sunset).format("LT")}</h6>
-                  </div>
-                  <div className="d-flex align-items-center desc-text my-3">
-                    <h6>{data.weather[0].description}</h6>
-                  </div>
-                  <div className="d-flex align-items-center mt-3">
-                    <h1 className="font-weight-bold rem-3">
-                      {Math.round(data.main.temp)}°C
-                    </h1>
-                  </div>
+                  <h6>Sunset: {moment.unix(data.sys.sunset).format("LT")}</h6>
                 </div>
-              ) : (
-                <div className="flex align-items-center justify-content-center">
-                  <div className="spinner-border" role="status"></div>
+                <div className="d-flex align-items-center desc-text my-3">
+                  <h6>{data.weather[0].description}</h6>
                 </div>
-              )}
-            </div>
+                <div className="d-flex align-items-center mt-3">
+                  <h1 className="font-weight-bold rem-3">
+                    {Math.round(data.main.temp)}°C
+                  </h1>
+                </div>
+                <div>
+                  <iframe
+                    frameBorder="0"
+                    className="map"
+                    src={`http://maps.google.com/maps?q=${data.coord.lat},${data.coord.lon}&z=12&output=embed`}
+                    height="450"
+                    allowFullScreen
+                    loading="lazy"
+                  ></iframe>
+                </div>
+              </div>
+            ) : (
+              <div className="flex align-items-center justify-content-center">
+                <div className="spinner-border" role="status"></div>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import GA4React, { useGA4React } from "ga-4-react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const ga4react = new GA4React("G-LDXRZM7MZ8");
+
+(async () => {
+  await ga4react.initialize();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+})();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
